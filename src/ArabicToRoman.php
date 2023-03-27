24e5b7d7de808e6aa4a12971186d9aad68686d0c
Facu <bestjayceeu@gmail.com>
2023-03-27 23:49:38 +0200

    ArabicToRoman kata php

--- src/ArabicToRoman.php
+++ src/ArabicToRoman.php
@@ -11,12 +11,25 @@ class ArabicToRoman
      *
      * @return string The roman number equivalent (e.g. CXXI)
      */
-    public static function transform(int $arabicNumber): string
+    public static function transform(int $num): ?string
     {
-        $romanNumber = '';
 
-        // Complete the function
+        if($num < 1) {
+            return null;
+        }
+
+        $romanTableValue = [["","I","II","III","IV","V","VI","VII","VIII","IX"],        
+        ["","X","XX","XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],   
+        ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"],
+        ["","M","MM","MMM"]];
+
+        $romanNumber = "";
+    
+        foreach (str_split(strrev(strval($num))) as $i => $e) {
+            $romanNumber = $romanTableValue[$i][intval($e)] . $romanNumber;
+        }
 
         return $romanNumber;
+        
     }
 }
